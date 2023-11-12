033449f7225c2ad8c26bab1bab99829f367a2351
Sina Fakhreddin <sina.fakhreddin@gmail.com>
2023-11-12 17:41:25 +0330

    feat:Drag and Drop Description

--- src/components/welcomeComponent/index.tsx
+++ src/components/welcomeComponent/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {NOTE_1, NOTE_2} from "../../constants";
 
 type Props = {
     setWelcomeModalShow:(show:boolean)=>void
@@ -7,7 +8,8 @@ type Props = {
 const WelcomeComponent = ({setWelcomeModalShow}:Props) => {
     return (
         <div className={'flex flex-col gap-4  justify-center px-4'}>
-            <span>This application uses Firebase, turn on your VPN for better performance. </span>
+            <span>{NOTE_1} </span>
+            <span>{NOTE_2}</span>
             <button onClick={()=>setWelcomeModalShow(false)} className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-700  sm:w-auto">
             Ok
             </button>
